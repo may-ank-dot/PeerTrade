a07db3e4b26883ea85ed7dbf7eb2f5b701dd3514
Mayank <albertnike1371@gmail.com>
2025-03-09 23:12:53 +0530

    added db query

---
+++ Back-End/db/init.js
@@ -0,0 +1,31 @@
+import db from "./connect.js";
+
+const createTables = async () => {
+    try{
+        await db.query(`
+            CREATE TABLE IF NOT EXISTS users(
+                id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                email VARCHAR(255) UNIQUE NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+        await db.query(`
+            CREATE TABLE IF NOT EXISTS listings(
+                id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+                title VARCHAR(255) NOT NULL,
+                description TEXT NOT NULL,
+                price FLOAT NOT NULL,
+                category VARCHAR(100) NOT NULL,
+                image_url TEXT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );     
+        `);
+        console.log("Tables created succesfully!");
+        } catch (error){
+            console.error(error);
+        } finally{
+            db.end();
+        }
+};
