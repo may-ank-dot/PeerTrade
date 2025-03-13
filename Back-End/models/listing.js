76b4c68004cd17ea787a7e4b86e971045fb81a1e
Mayank <albertnike1371@gmail.com>
2025-03-13 12:51:00 +0530

    listing CRUD setup

--- Back-End/models/listing.js
+++ Back-End/models/listing.js
@@ -3,7 +3,7 @@ import db from "../db/connect.js";
 const createListings = async (title, description, price, category , image_url) => {
     try{
         const result = await db.query(
-            "INSERT INTO listings(title,description,price,category,image_url) VALUES($1,$2,$3,$4,$5) RETURNIG *"
+            "INSERT INTO listings(title,description,price,category,image_url) VALUES($1,$2,$3,$4,$5) RETURNING *"
             ,[title , description, price,category,image_url]
         );
         return result.rows[0];
@@ -15,10 +15,41 @@ const createListings = async (title, description, price, category , image_url) =
 const getAllListings = async () => {
     try{
         const result = await db.query("SELECT * FROM listings");
-        return result.rows[0];
+        return result.rows;
     } catch(error){
-        console.error("Something went wrong",error);
+        console.error("Get listing not working",error);
     }
 } 
 
-export {createListings , getAllListings};
+const getListingById = async (id) => {
+    try{
+        const result = await db.query("SELECT * FROM listings WHERE id = $1",[id]);
+        return result.rows[0];
+    } catch(error){
+        console.error("Listing by id not working",error);
+    }
+} 
+
+const updateListingById = async (id,title,description,price,category,image_url) => {
+    try{
+        const result = 
+            await db.query(
+                "UPDATE listings SET title=$1,description=$2,price=$3,category=$4,image_url=$5 WHERE id=$6 RETURNING *",
+            [title,description,price,category,image_url,id]);
+            return result.rows[0];
+    } catch(error){
+        console.error("Error while updating Listings",error);
+    }
+}
+
+const deleteListings = (id) => {
+    try{
+        const result = db.query("DELETE FROM listings WHERE id=$1 ",[id]);
+        return result.rows;
+    }catch(error) {
+        console.error("error while deleting listing",error);
+    }
+}
+
+// Reporting inappropriate listing (not now)
+export {createListings , getAllListings,getListingById,deleteListings,updateListingById};
--- Back-End/routes/listingRoutes.js
+++ Back-End/routes/listingRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListings,getAllListings } from "../models/listing.js";
+import { createListings,getAllListings,deleteListings,getListingById,updateListingById} from "../models/listing.js";
 
 const router = express.Router();
 
@@ -22,4 +22,34 @@ router.get("/",async(req,res) => {
     }
 });
 
+router.get("/:id",async(req,res)=>{
+    try{
+        const id = req.params;
+        const lisiting = await getListingById(id.id); 
+        res.status(201).json(lisiting);
+    } catch(error){
+        res.status(500).json({error:"error getting lsiting by id"});
+    }
+});
+
+router.put("/update/:id",async(req,res) => {
+    try{
+        const id = req.params;
+        const {title,description,price,category,image_url} = req.body;
+        const updated = await updateListingById(id.id,title,description,price,category,image_url);
+        res.status(201).json(updated);
+    } catch (error) {
+        console.error("Updating listing failed",error);
+    }
+});
+
+router.delete("/delete/:id",async(req,res) => {
+    try{
+        const id = req.params;
+        const deleted = await deleteListings(id.id);
+        res.status(201).json({message:"Listing deleted successfully"})
+    } catch(error){
+        console.error("Error deleting listing",error);
+    }
+})
 export default router;
