04dc91d952fb2bdf7bcf59f7cfa374bbf0aa90aa
Mayank <albertnike1371@gmail.com>
2025-03-15 23:14:11 +0530

    Home

--- Front-End/src/pages/Home.jsx
+++ Front-End/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Home = () => {
-    return(<div></div>)
+    return(<div>
+        <Header>
+
+        </Header>
+        <div>
+
+        </div>
+    </div>)
 }
 
 export default Home;
