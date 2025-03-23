416a22097102403309897b1e70115ca48b8f5fef
Mayank <albertnike1371@gmail.com>
2025-03-23 20:54:48 +0530

    Added login pages

--- Front-End/src/pages/Login.jsx
+++ Front-End/src/pages/Login.jsx
@@ -1,20 +1,47 @@
 import React,{useState} from "react";
+import API from "../services/api";
+import {useNavigate} from "react-router-dom";
 
 const Login = () => {
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
+    const [loading,setLoading] = useState(false);
+    const [error,setError] = useState("");
+    const navigate = useNavigate();
+
     const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log({email,password});
+        setLoading(true);
+        setError("");
+
+        try{
+           const response = await API.post('/users/login',{email,password});
+           console.log("login Success",response.data); 
+           // To store token in local storage
+           localStorage.setItem("token",response.data.token);
+           console.log(response);
+           navigate("/listings");
+        }catch(err){
+            setError("Invalid email or password. Please try again.");
+            console.error("Login Error",err);
+        }finally{
+            setLoading(false);
+        }
     }
+    
     return(
         <div className="flex justify-center items-center h-screen bg-amber-200">
-            <form onSubmit={handleSubmit}>
-                <label>Email</label>
-                <input className="border-gray-600 bg-white" type="text" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                <label>Password</label>
-                <input className="border-gray-600 bg-white" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                <button className="border-gray-600 rounded-b-md" type="submit">Submit</button>
+            <form onSubmit={handleSubmit} className="flex flex-col bg-white px-20 py-8 shadow-md rounded-md">
+                <h2 className="text-xl font-bold mb-4">Login</h2>
+                {error && <p className="text-red-500">{error}</p>}
+                <label className="mb-1">Email</label>
+                <input className="border border-gray-600 rounded mb-3" type="text" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                <label className="mb-1">Password</label>
+                <input className="border border-gray-600 rounded mb-3" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <button className="border border-gray-600 rounded-md bg-blue-500 text-white px-2 py-2 mt-2 hover:bg-red-500" type="submit" disabled={loading}>
+                    {loading ? "Loging in.." : "login"}
+                
+                </button>
             </form>
         </div>
     )
--- Front-End/src/services/api.jsx
+++ Front-End/src/services/api.jsx
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const API = axios.create({
     baseURL:"http://localhost:3000",
-    withCredentials: true
+    headers:{
+        "Content-Type": "application/json",
+    },
 })
 
 export default API;
